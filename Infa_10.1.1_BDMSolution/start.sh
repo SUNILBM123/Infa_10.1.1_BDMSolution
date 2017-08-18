cc1cd5f69387d7fcf09a0f5f06a0fb14ae7a47c9
SUNILBM123 <sunilm@informatica.com>
2017-08-18 17:58:56 +0530

    check for ssh option

---
+++ Infa_10.1.1_BDMSolution/start.sh
@@ -0,0 +1,2 @@
+cd /home/suniluser/reconfiguration
+echo "Administrator@123" | sudo -S sh hdireconfigure.sh sunilhdicluster admin "Bdmrndhdcluster@123" sunilhdicluster-ssh.azurehdinsight.net hdssh "Bdmrndhdcluster@123" /tmp/blazeworkdir /tmp/spark/workdir /tmp/spark/eventdir suniluser "Administrator@123" vm1 Administrator "Administrator@123" /home/Informatica/10.1.1 "Azure_Domain" > output.txt
